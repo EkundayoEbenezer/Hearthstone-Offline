01ff081df2f7511b8cbea4228ac64e91aee6cd88
EkundayoEbenezer <Ebenezertopzy@gmail.com>
2024-06-10 02:16:41 -0900

    first commit

---
+++ Assets/Scripts/SO Asset Scripts/CardAsset/CardAsset.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TargetingOptions
+{
+    NoTarget,
+    AllCreatures, 
+    EnemyCreatures,
+    YourCreatures, 
+    AllCharacters, 
+    EnemyCharacters,
+    YourCharacters
+}
+
+public class CardAsset : ScriptableObject 
+{
+    // this object will hold the info about the most general card
+    [Header("General info")]
+    public CharacterAsset characterAsset;  // if this is null, it`s a neutral card
+    [TextArea(2,3)]
+    
+
+    public string cardName;
+    public string Description;  // Description for spell or character
+	public Sprite CardImage;
+    public int ManaCost;
+    public bool ArmorPiercing;
+
+    [Header("Creature Info")]
+    public int MaxHealth;
+    public int Attack;
+    public int AttacksForOneTurn = 1;
+    public bool hasTaunt;
+    public bool Charge;
+    public string CreatureScriptName;
+    public int specialCreatureAmount;
+	public int Armor;
+
+    [Header("SpellInfo")]
+    public string SpellScriptName;
+    public int specialSpellAmount;
+    public TargetingOptions Targets;
+
+}
